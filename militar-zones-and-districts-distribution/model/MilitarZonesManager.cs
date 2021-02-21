123e85db92ae145b98bf2be892474e90c6317947
Marco Vasquez <macroxx120@gmail.com>
2021-02-20 20:11:41 -0500

    created class MilitarZonesManager and methods to load the data and initialize the dataTable

---
+++ militar-zones-and-districts-distribution/model/MilitarZonesManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+
+namespace militar_zones_and_districts_distribution.model
+{
+    class MilitarZonesManager
+    {
+        private List<MilitarZoneOrDistrict> militarZones;
+        private DataTable dataTable;
+
+
+        public MilitarZonesManager()
+        {
+            militarZones = new List<MilitarZoneOrDistrict>();
+            dataTable = new DataTable();
+        }
+
+        public void LoadZones(String path)
+        {
+            var reader = new StreamReader(File.OpenRead(@path));
+            string line = reader.ReadLine();
+
+            string[] headers = line.Split(',');
+            GenerateTable(headers);
+
+            line = reader.ReadLine();
+
+            while (line!=null && !line.Equals(""))
+            {
+                line = line.Trim(new Char[] {'(',')','"',});
+                Console.WriteLine(line);
+                string[] info = line.Split(',');
+                
+                int zoneNumber = System.Convert.ToInt32(info[0]);
+                string zoneDistrict = info[1];
+                string address = info[2];
+                string city = info[3];
+                string phoneNumber = info[4];
+                double latitude = System.Convert.ToDouble(info[5]);
+                double longitude = System.Convert.ToDouble(info[6]);
+
+                Console.WriteLine(zoneNumber+ "," + zoneDistrict + "," + address + "," + city + "," + phoneNumber +","+latitude+","+longitude);
+                MilitarZoneOrDistrict toAdd = new MilitarZoneOrDistrict(zoneNumber, zoneDistrict, address, city, phoneNumber, latitude, longitude);
+
+                militarZones.Add(toAdd);
+                line = reader.ReadLine();
+            }
+        }
+
+        public void GenerateTable(String[] headers)
+        {
+            foreach (string head in headers)
+            {
+                this.dataTable.Columns.Add(head);
+            }
+        }
+
+        public DataTable LoadTable()
+        {
+            dataTable.Rows.Clear();
+            
+
+            return null;
+        }
+
+
+    }
+}
